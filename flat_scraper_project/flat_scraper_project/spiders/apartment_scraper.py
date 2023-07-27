de7429e7a442db18d6b2384b8a0bbbf7be128208
Nik-tech-cloud <62603376+Nik-tech-cloud@users.noreply.github.com>
2023-07-28 00:31:51 +0200

    simple spider for two sites to get the html - this code will be removed later

--- flat_scraper_project/flat_scraper_project/spiders/apartment_scraper.py
+++ flat_scraper_project/flat_scraper_project/spiders/apartment_scraper.py
@@ -1,2 +1,18 @@
+import scrapy
+from pathlib import Path
 class ApartmentScraper(scrapy.Spider):
     name = 'apartments'
+
+    def start_requests(self):
+        urls = [
+            'https://www.sreality.cz/en/search/for-sale/apartments',
+            "https://www.sreality.cz/en/search/for-sale/apartments?page=2",
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+    
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = f"quotes-{page}.html"
+        Path(filename).write_bytes(response.body)
+        self.log(f"Saved file {filename}")

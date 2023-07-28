87b54baced11c98e9b73104ac8ebe0e74790d600
Nik-tech-cloud <62603376+Nik-tech-cloud@users.noreply.github.com>
2023-07-28 21:29:21 +0000

    Robots.txt file ignored

--- flat_scraper_project/flat_scraper_project/settings.py
+++ flat_scraper_project/flat_scraper_project/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "flat_scraper_project.spiders"
 #USER_AGENT = "flat_scraper_project (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32

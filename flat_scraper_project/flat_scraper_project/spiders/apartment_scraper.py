eb9746730de829b446ed83e3bc171784d478a068
Nik-tech-cloud <62603376+Nik-tech-cloud@users.noreply.github.com>
2023-07-30 20:58:02 +0000

    Sprider implementation using playwright and getting requred data

--- flat_scraper_project/flat_scraper_project/spiders/apartment_scraper.py
+++ flat_scraper_project/flat_scraper_project/spiders/apartment_scraper.py
@@ -1,23 +1,38 @@
 import scrapy
 from pathlib import Path
-from selenium import webdriver
+from time import sleep
 
 
 class ApartmentScraper(scrapy.Spider):
     name = 'apartments'
 
-    def __init__(self):
-            self.driver = webdriver.Firefox()
-
     def start_requests(self):
-        urls = [
-            'https://www.sreality.cz/en/search/for-sale/apartments'
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        url = 'https://www.sreality.cz/en/search/for-sale/apartments'
+        yield scrapy.Request(url=url, callback=self.parse, meta={"playwright": True})
+        self.crawler.stats.set_value('apartments_scraped', 0)
     
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f"flats-{page}.html"
-        Path(filename).write_bytes(response.body)
-        self.log(f"Saved file {filename}")
+        sleep(5)
+        for apartment in response.css('.property.ng-scope'):
+            title = apartment.css('.name.ng-binding::text').get()
+            self.log(title)
+            location = apartment.css('.locality.ng-binding::text').get()
+            self.log(location)
+            price = apartment.css('.norm-price.ng-binding::text').get()
+            self.log(price)
+            images = []
+            for image in apartment.css('preact.ng-scope.ng-isolate-scope a > img::attr(src)'):
+                image_src = image.get()
+                images.append(image_src)
+                self.log(image_src)
+            self.crawler.stats.inc_value('apartments_scraped')
+            self.log('total apartments scraped: ' + str(self.crawler.stats.get_value('apartments_scraped')))
+            self.log('------------------')
+        
+        if self.crawler.stats.get_value('apartments_scraped') < 500:
+            button_href = response.css('a.btn-paging-pn.paging-next::attr(href)').get()
+            next_button_url = 'https://www.sreality.cz' + button_href
+            yield scrapy.Request(url=next_button_url, callback=self.parse, meta={"playwright": True})
+
+                
+            
